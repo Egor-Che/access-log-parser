b8f7776a74ee526d67d78d358cf547d1fd772c70
Egor Chichin <chichin@inbox.ru>
2025-02-16 19:36:20 +0300

    Added math functions to Main file

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+
+        double result1 = number1 + number2;
+        double result2 = number1 - number2;
+        double result3 = number1 * number2;
+        double result4 = (double) number1 / number2;
+
+        System.out.println("Сумма введенных чисел равна: " + result1);
+        System.out.println("Разность введенных чисел равна: " + result2);
+        System.out.println("Произведение введенных чисел равно: " + result3);
+        System.out.println("Частное введенных чисел равно: " + result4);
+
     }
 }
